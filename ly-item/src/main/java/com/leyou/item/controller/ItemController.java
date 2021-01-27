19286f7adae714ba3a8e0bc717ada35799f5cfe0
cbh <2801919169@qq.com>
2021-01-27 14:47:29 +0800

    乐优商城分类

--- ly-item/src/main/java/com/leyou/item/controller/ItemController.java
+++ ly-item/src/main/java/com/leyou/item/controller/ItemController.java
@@ -1,4 +1,4 @@
-package com.leyou.item.ItemController;
+package com.leyou.item.controller;
 
 import com.leyou.item.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
